04777d10e4dbbecc0ffd3f2004a87da4fbae7a98
Praveen Murali <itspraveenmurali@gmail.com>
2025-11-15 17:48:16 +0530

    Add icons

--- example/src/Dashboard/IconsList/IconItem.js
+++ example/src/Dashboard/IconsList/IconItem.js
@@ -7,14 +7,14 @@ const IconItem = ({ name, Icon, onSelect, isSelected, size = 36 }, ref) => {
       ref={ref}
       onClick={() => onSelect(name)}
       className={classNames(
-        "flex flex-col items-center justify-center col-span-1 p-8 transition-all transform bg-white border rounded cursor-pointer hover:bg-gray-100",
+        "flex flex-col items-center justify-center col-span-1 p-2 transition-all transform bg-white border rounded cursor-pointer hover:bg-gray-100 w-full h-20",
         {
           "border-blue-500": isSelected,
         }
       )}
     >
       <Icon {...{ size }} color="#68737D" />
-      <div className="mt-3 text-xs text-gray-700">{name}</div>
+      <div className="mt-3 text-xs text-gray-700 truncate w-full text-center">{name}</div>
     </div>
   );
 };
